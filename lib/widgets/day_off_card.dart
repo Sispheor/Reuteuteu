e52799d3b880b8b006a51deec5acf862831dd38f
Nicolas Marcq <nico.marcq@gmail.com>
2022-04-23 21:23:32 +0200

    add timeline

--- lib/widgets/day_off_card.dart
+++ lib/widgets/day_off_card.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:reuteuteu/models/day_off.dart';
 import 'package:reuteuteu/models/pool.dart';
 import 'package:reuteuteu/pages/create_or_edit_day_off.dart';
-import 'package:reuteuteu/widgets/date_card.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 
 class DayOffCardWidget extends StatefulWidget {
 
@@ -29,15 +30,19 @@ class _DayOffCardWidgetState extends State<DayOffCardWidget> {
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             ListTile(
-              leading: CircleAvatar(
-                  backgroundColor: Colors.green,
-                  child: Text(widget.dayOff.getTotalTakenDays().toString(),
-                      style: const TextStyle(color: Colors.white))),
+              leading: Container(
+                  child: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.green,
+                      child: Text(widget.dayOff.getTotalTakenDays().toString(),
+                          style: const TextStyle(color: Colors.white)))
+              ),
               title: Text(widget.dayOff.name),
-              subtitle: widget.dayOff.isHalfDay? const Text(
-                "Half day(s)",
-                style: TextStyle(color: Colors.black),
-              ): null,
+              // subtitle: widget.dayOff.isHalfDay? const Text(
+              //   "Half day(s)",
+              //   style: TextStyle(color: Colors.black),
+              // ): null
+              subtitle: _TimelineTileStartEnd(dayOff: widget.dayOff),
               trailing: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -51,30 +56,6 @@ class _DayOffCardWidgetState extends State<DayOffCardWidget> {
                   }, icon: const Icon(Icons.delete)),
                 ],
               ),
-            ),
-            Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          DateCard(widget.dayOff.dateStart),
-
-                          if (widget.dayOff.getTotalTakenDays() >= 1)
-                            const Icon(
-                              Icons.arrow_forward,
-                              color: Colors.green,
-                              size: 30.0,
-                            ),
-                          if (widget.dayOff.getTotalTakenDays() >= 1)
-                            DateCard(widget.dayOff.dateEnd),
-                        ],
-                      ),
-
-                    ]
-                )
             ),
           ],
         ),
@@ -115,5 +96,116 @@ class _DayOffCardWidgetState extends State<DayOffCardWidget> {
         );
       },
     );
+  }
+}
+
+
+class _TimelineTileStartEnd  extends StatelessWidget {
+
+  const _TimelineTileStartEnd({
+    Key? key,
+    required this.dayOff,
+  }) : super(key: key);
+
+  final DayOff dayOff;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Row(
+        children: [
+          Expanded(
+              child: Column(
+                  children: [
+                    TimelineTile(
+                      alignment: TimelineAlign.manual,
+                      lineXY: 0.08,
+                      isFirst: true,
+                      indicatorStyle: IndicatorStyle(
+                        width: 30,
+                        height: 30,
+                        color: Colors.green,
+                        iconStyle: IconStyle(
+                          color: Colors.white,
+                          iconData: Icons.calendar_today,
+                        ),
+                      ),
+                      beforeLineStyle: LineStyle(
+                        color: _getColorIfHalday(dayOff),
+                        thickness: 4,
+                      ),
+                      endChild: _Child(
+                        text: DateFormat('dd MMMM yyyy').format(dayOff.dateStart),
+                      ),
+                    ),
+
+                    if (dayOff.getTotalTakenDays() > 1)
+                      TimelineTile(
+                        alignment: TimelineAlign.manual,
+                        lineXY: 0.08,
+                        isLast: true,
+                        indicatorStyle: IndicatorStyle(
+                          width: 30,
+                          height: 10,
+                          color: Colors.green,
+                          iconStyle: IconStyle(
+                            color: Colors.white,
+                            iconData: Icons.calendar_today,
+                          ),
+                        ),
+                        beforeLineStyle: const LineStyle(
+                          color: Colors.green,
+                          thickness: 4,
+                        ),
+                        endChild: _Child(
+                          text: DateFormat('dd MMMM yyyy').format(dayOff.dateEnd),
+                        ),
+                      ),
+                  ]
+              ))]
+    );
+  }
+
+  _getColorIfHalday(DayOff isHalfDay) {
+    if (dayOff.getTotalTakenDays() > 1){
+      return Colors.green;
+    }
+    return Colors.white;
+  }
+
+}
+
+class _Child extends StatelessWidget {
+  const _Child({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+        constraints: const BoxConstraints(
+          minHeight: 50,
+          maxHeight: 100,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          // color: Colors.amberAccent,
+          // constraints: const BoxConstraints(maxHeight: 40),
+          child:
+          Align(
+            alignment: Alignment.centerLeft, // Align however you like (i.e .centerRight, centerLeft)
+            child: Text(
+              text,
+              textAlign: TextAlign.start,
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+        )
+    );
+
   }
 }
