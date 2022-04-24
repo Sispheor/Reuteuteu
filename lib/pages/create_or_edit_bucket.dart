74258437528fbf9aa64ca7e4bd71d8f3345d5d68
Nicolas Marcq <nico.marcq@gmail.com>
2022-04-24 19:24:46 +0200

    CRUD on buckets

---
+++ lib/pages/create_or_edit_bucket.dart
@@ -0,0 +1,123 @@
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:reuteuteu/hive_boxes.dart';
+import 'package:reuteuteu/models/bucket.dart';
+
+class CreateOrEditBucketPage extends StatefulWidget {
+
+  final bool isEdit;
+  final Bucket? bucket;
+
+  const CreateOrEditBucketPage({
+    Key? key,
+    required this.isEdit, this.bucket
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CreateOrEditDayOffPage();
+  }
+
+}
+
+class _CreateOrEditDayOffPage  extends State<CreateOrEditBucketPage> {
+  String title = "Create a new bucket";
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    if (widget.isEdit){
+      nameController.text = widget.bucket!.name;
+    }else{
+      nameController.text = _getDefaultBucketName();
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Form(key: _formKey,
+                      child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  border: UnderlineInputBorder(),
+                                  labelText: 'Name',
+                              ),
+                              controller: nameController,
+                              // The validator receives the text that the user has entered.
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter a name';
+                                }
+                                if (_isBucketNameExist(value)){
+                                  return 'Bucket name exist already';
+                                }
+                                return null;
+                              },
+                            ),
+                          ]
+                      )
+                  )
+                ]
+            )
+        ),
+        floatingActionButton: FloatingActionButton(
+          // When the user presses the button, show an alert dialog containing
+          // the text that the user has entered into the text field.
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              if (widget.isEdit){
+                await _updateBucket(widget.bucket!);
+              }else{
+                await _persistBucket();
+              }
+              Navigator.pop(context);  // return to previous screen (main)
+            }
+          },
+          child: const Icon(Icons.done),
+          backgroundColor: Colors.green,
+        )
+    );
+
+  }
+
+  _persistBucket() {
+    Bucket newBucket = Bucket(nameController.text);
+    final box = Boxes.getBuckets();
+    box.add(newBucket);
+  }
+
+  String _getDefaultBucketName() {
+    DateTime now = DateTime.now();
+    DateTime nowPlusOneYear = DateTime(now.year + 1);
+    String formatteYearNow = DateFormat('yyyy').format(now);
+    String formatterNowPlusOneYear = DateFormat('yyyy').format(nowPlusOneYear);
+    String finalDate = "$formatteYearNow/$formatterNowPlusOneYear";
+    return finalDate;
+  }
+
+  bool _isBucketNameExist(String value) {
+    final box = Boxes.getBuckets();
+    for (Bucket existingBucket in box.values.toList().cast<Bucket>()){
+      if (value == existingBucket.name){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  _updateBucket(Bucket bucket) {
+    bucket.name = nameController.text;
+    bucket.save();
+  }
+}
--- lib/widgets/bucket_card.dart
+++ lib/widgets/bucket_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:reuteuteu/models/bucket.dart';
+import 'package:reuteuteu/pages/create_or_edit_bucket.dart';
 import 'package:reuteuteu/pages/list_pool.dart';
+import 'package:reuteuteu/widgets/dialog_confirm_cancel.dart';
 
-class BucketCardWidget extends StatelessWidget {
+class BucketCardWidget extends StatefulWidget {
 
   const BucketCardWidget({
     Key? key,
@@ -12,6 +14,11 @@ class BucketCardWidget extends StatelessWidget {
   final Bucket bucket;
 
   @override
+  State<BucketCardWidget> createState() => _BucketCardWidgetState();
+}
+
+class _BucketCardWidgetState extends State<BucketCardWidget> {
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: Card(
@@ -19,12 +26,29 @@ class BucketCardWidget extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             ListTile(
-              title: Text(bucket.name),
+              title: Text(widget.bucket.name),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => CreateOrEditBucketPage(isEdit: true, bucket: widget.bucket))
+                    ).then((_) => setState(() {}));
+                  }, icon: const Icon(Icons.edit)),
+                  IconButton(onPressed: () async {
+                    final action = await ConfirmCancelDialogs.yesAbortDialog(context, "Delete bucket '${widget.bucket.name}'?", 'Confirm');
+                    if (action == DialogAction.confirmed) {
+                      widget.bucket.delete();
+                      // widget.callback();
+                    }
+                  }, icon: const Icon(Icons.delete)),
+                ],
+              ),
               onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) {
-                      return ListPoolPage(bucket: bucket);
+                      return ListPoolPage(bucket: widget.bucket);
                     }),
                   );
               },
